7309229a5f50d8220df2d5b9ac91593adc1273b5
n136k793 <n136k793@kumc.edu>
2017-09-12 18:32:03 -0500

    Modified check_agreement_signed() to use more of existing plugin framework

--- UtilityFunctions.php
+++ UtilityFunctions.php
@@ -1,59 +1,91 @@
 <?php
 
+/*
+Function to append user info to the survey link
+
+@param string $output_survey: 
+	REDCap survey url to which user details need to be appended
+@param array $user_info_array: 
+	Associative array that has user details in 
+	field_name => field_value format 
+	which should be appended to the survey url
+
+This function,
+- iteratively adds all the field names and values to url,
+- replaces space character with '+' to form a continous url,
+- and returns the appended url. 
+*/
 
 function add_surveylink_userinfo($output_survey,$user_info_array){
 
-		$user_details = null;
+	$user_details = null;
 
-		foreach($user_info_array as $field_name => $field_value){
+	foreach($user_info_array as $field_name => $field_value){
 		
-			$user_details = $user_details.'&'.$field_name.'='.$field_value; 
+		$user_details = $user_details.'&'.
+				$field_name.'='.$field_value; 
 		 	
-		}
+	}
 
-                $new_link = $output_survey.''.$user_details;
+        $new_link = $output_survey.''.$user_details;
+        $new_link_no_spaces = str_replace(' ','+',$new_link);
 
-                $new_link_no_spaces = str_replace(' ','+',$new_link);
+        return $new_link_no_spaces;
 
-               // echo $new_link_no_spaces;
+}
 
-                return $new_link_no_spaces;
+/*	
+Function to generate unique survey link specific to a record using REDCap API
 
-        }
-	
+@param string api_url: 
+	REDCap API url
+@param string record_num: 
+	Record id for which survey link should be generated
+@param string survey_name: 
+	Survey name for which link should be generated.
+@param string post_token: 
+	API token that is needed to access the specfic REDCap project 
 
+This function
+- Creates RestCallRequest object
+- Executes the request
+- Returns tha response to the calling function if the http-code 200 else
+  decodes the error message and returns it to the calling method.
+*/
 
 function generate_survey_link($url,$record_num, $survey_name, $post_token) {
 
-		require_once(FRAMEWORK_ROOT.'/RestCallRequest.php');
+	require_once(FRAMEWORK_ROOT.'/RestCallRequest.php');
         	
-        	$api_request = new RestCallRequest(
-	        	$url,
-                	'POST',
-                 	array(
-             	        	'content'       => 'surveyLink',
-                      		'format'        => 'json',
-                       		'instrument'    => $survey_name,
-                       		'record'        => $record_num,
-                       		'token'         => $post_token
-               		)
-        	);
+        $api_request = new RestCallRequest(
+	        $url,
+                'POST',
+                 array(
+             	        'content'       => 'surveyLink',
+                      	'format'        => 'json',
+                       	'instrument'    => $survey_name,
+                       	'record'        => $record_num,
+                       	'token'         => $post_token
+               	)
+        );
 
-	        $api_request->execute();
-        	$response_info = $api_request->getResponseInfo();
-        	//$api_response = $api_request->getResponseBody();
-        	$error_msg = '';
-        	if($response_info['http_code'] == 200) {
-        		$api_response = $api_request->getResponseBody();
-             		return $api_response;
-        	} else {
-        		$api_response = json_decode($api_request->getResponseBody(), true);
-                	$error_msg = (isset($api_response['error'])
-                        	        ? $api_response['error']
-                                	: 'No error returned.');
-                	return array(false, $error_msg);
-        	}	
-	}
+	$api_request->execute();
+        $response_info = $api_request->getResponseInfo();
+        $error_msg = '';
+        
+	if($response_info['http_code'] == 200) {
+        	$api_response = $api_request->getResponseBody();
+             	return $api_response;
+        } else {
+        	$api_response = json_decode($api_request->getResponseBody(), 
+				true);
+	
+                $error_msg = (isset($api_response['error'])
+                        	    ? $api_response['error']
+                                    : 'No error returned.');
+                return array(false, $error_msg);
+        }	
+}
 
  /* check_agreement_signed() is a common method for all the
         projects that verifies if the user_id already exists for
@@ -64,75 +96,40 @@ function generate_survey_link($url,$record_num, $survey_name, $post_token) {
         */
 
 
- function check_agreement_signed($survey,
-        	         $userid_field_survey_label,$kumc_username_value,
-                	 $heronParticipants,$record_id_label,$pid){
+function check_agreement_signed($survey,$userid_field,$userid_value,
+               	 		$heronParticipants,$rec_id_label,$pid){
 	
-		require_once(REDCAP_ROOT.'redcap_connect.php');
-		$matchedRecords = REDCap::getData(
-        		$pid,
-                	'array',
-                	null,
-                	$userid_field_survey_label
-         	);
 
-	       	$existingUserIds = array();
+	$records_info = $heronParticipants->get_records_by($userid_field,
+						$userid_value);
+        $survey_complete_field = $survey.'_complete';
+        	
+	if(count($records_info)!= 0){
+	
+		$rec_num = $heronParticipants->get_record_ids_by(
+						$userid_field,
+                                                $userid_value,
+                                                true);
 
-         	foreach ($matchedRecords as $recordNum)
-         	{
-         		foreach($recordNum as $eventNum)
-                	{
-                		foreach($eventNum as $uid_field => $uid_value )
-                        	{
-                        		array_push($existingUserIds,$uid_value);
-                        	}
-                 	}	
-          	}	
+		$rec =  max($rec_num);
 
-          	
-          	$survey_complete_field = $survey.'_complete';
-		echo $survey_complete_field;
-		echo $kumc_username_value;
-	//	var_dump($existingUserIds);
-		echo "\n record label is $record_id_label\n";
+		$userid_latest_rec = $heronParticipants->get_record_by(
+							'record',$rec);
 
-          	if(in_array($kumc_username_value,$existingUserIds)){
+		if($userid_latest_rec[$survey_complete_field]==2){
+      	               	return array('survey_complete' => true,
+				'record_num' => $userid_latest_rec[$rec_id_label]);
 
-
-          		$record_having_userid = $heronParticipants->get_record_by(
-                        	                                $userid_field_survey_label,
-                                	                        $kumc_username_value,
-		                           	                $event_id=null);
-
-			var_dump($record_having_userid);
-			echo "\n Value in survey complete field is:$record_having_userid[0][$survey_complete_field]";
- 
-          		if($record_having_userid[$survey_complete_field]==2){
-	          		
-                  		echo  $record_having_userid[$record_id_label];
-				return array('survey_complete' => true,
-					     'record_num' => $record_having_userid[$record_id_label]);
-
-          		}else{
-				echo "\n User id present but survey is not completed";
-				return array('survey_complete' => false,
-					     'record_num' => $record_having_userid[$record_id_label]);
-
-			}
           	}else{
-
-          		return array('survey_complete' => false,
-				     'record_num' => null);
+			return array('survey_complete' => false,
+				'record_num' => $userid_latest_rec[$rec_id_label]);
 		}
-
-		//register_shutdown_function('shutDownFunction');
+          
+	}else{
+          	return array('survey_complete' => false,
+			'record_num' => null);
+	}
 
 }
 
-
-function shutDownFunction()
-{
-    $error = error_get_last();
-    print_r($error,true);
-}
 ?>
--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
 
 /**
- * PLUGIN NAME: MOCK-REDCap Plugin for Heron Admin
+ * PLUGIN NAME: Authenticated Surveys Plugin 
  * DESCRIPTION: Developed by the University of Kansas Medical Center's Medical
- *              Informatics Department for integrating Heron users with Redcap
+ *              Informatics Department to generate secure survey links 
+		for integrating other applications like HERON with Redcap
  * VERSION: 1.0
  * AUTHOR: Michael Prittie, Nazma Sulthana
  */
 
-// Retrieve REDCap global MYSQLi database connection object (REDCapism)
+// Provides access to REDCap helper functions and database connection.
+//(REDCapism)
 global $conn;
-//echo "inside starting index";
+
+// Report all PHP errors
 error_reporting(E_ALL);
+
+// Skipping the REDCap login authentication
 define ("NOAUTH", true);
 
+// Defining the REDCap root directory path
 $redcap_root_path = get_redcap_root();
 define('REDCAP_ROOT', $redcap_root_path.'/');
+
+// Need redcap_connect.php to connect this plugin to REDCap framework
 require_once(REDCAP_ROOT.'redcap_connect.php');
 
-define('FRAMEWORK_ROOT', REDCAP_ROOT.'plugins/framework/');
-require_once(FRAMEWORK_ROOT.'PluginConfig.php');
-
-
+// Defining the authenticated surveys plugin root path
 define('AUTH_SURVEY_ROOT',REDCAP_ROOT.'plugins/authenticated_surveys/');
 
+// Load configuration plugin configuration
+define('FRAMEWORK_ROOT', REDCAP_ROOT.'plugins/framework/');
+require_once(FRAMEWORK_ROOT.'PluginConfig.php');
 $CONFIG = new PluginConfig(AUTH_SURVEY_ROOT.'config.ini');
+
+// Class SurveyController's object handles the incoming HTTP requests
 require_once(AUTH_SURVEY_ROOT.'SurveyController.php');
 
 $controller = new SurveyController(  
@@ -37,8 +47,17 @@ $controller = new SurveyController(
 
 $result = $controller->process_request();
 
+// Displaying the reponse of the processed request
 var_dump($result);
 
+
+/*
+get_redcap_root() determines the REDCap ROOT path.
+
+From the current directory, iteratively checks in the parent directory
+for the files install.php, redcap_connect.php and database.php
+to determine if that location is the REDCap root.
+*/
 function get_redcap_root() {
 	$dir = dirname(__FILE__);
         while (!file_exists($dir.'/redcap_connect.php') && 
@@ -53,6 +72,5 @@ function get_redcap_root() {
         }
 	exit;
 }
-
 
 ?>
